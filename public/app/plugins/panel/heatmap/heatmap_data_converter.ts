syunkitada/grafana-7517

commit 378745a6e7cee7fd228d1cf86b8ee48aa1c22c17

allow heatmap parsing of scaled datapoints (#8632)

public/app/plugins/panel/heatmap/heatmap_data_converter.ts
@@ -207,15 +207,20 @@ function pushToXBuckets(buckets, point, bucketNum, seriesName) {
 }
 
 function pushToYBuckets(buckets, bucketNum, value, point, bounds) {
+  var count = 1;
+  // Use the 3rd argument as scale/count
+  if (point.length > 2) {
+    count = parseInt(point[2]);
+  }
   if (buckets[bucketNum]) {
     buckets[bucketNum].values.push(value);
-    buckets[bucketNum].count += 1;
+    buckets[bucketNum].count += count;
   } else {
     buckets[bucketNum] = {
       y: bucketNum,
       bounds: bounds,
       values: [value],
-      count: 1,
+      count: count,
     };
   }
 }

commit 021828be78e6de8777b8d91df28e681bd4f33e77

fix heatmap count values bug introduced by #8632

public/app/plugins/panel/heatmap/heatmap_data_converter.ts
@@ -192,16 +192,16 @@ function pushToXBuckets(buckets, point, bucketNum, seriesName) {
   if (value === null || value === undefined || isNaN(value)) { return; }
 
   // Add series name to point for future identification
-  point.push(seriesName);
+  let point_ext = _.concat(point, seriesName);
 
   if (buckets[bucketNum] && buckets[bucketNum].values) {
     buckets[bucketNum].values.push(value);
-    buckets[bucketNum].points.push(point);
+    buckets[bucketNum].points.push(point_ext);
   } else {
     buckets[bucketNum] = {
       x: bucketNum,
       values: [value],
-      points: [point]
+      points: [point_ext]
     };
   }
 }
@@ -209,7 +209,7 @@ function pushToXBuckets(buckets, point, bucketNum, seriesName) {
 function pushToYBuckets(buckets, bucketNum, value, point, bounds) {
   var count = 1;
   // Use the 3rd argument as scale/count
-  if (point.length > 2) {
+  if (point.length > 3) {
     count = parseInt(point[2]);
   }
   if (buckets[bucketNum]) {